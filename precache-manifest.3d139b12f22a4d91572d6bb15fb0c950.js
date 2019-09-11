0bba2b13071faecc0e34fa91c032cb6ada01fa00
CN0103 <cn0103@CN103.local>
2019-09-11 14:58:37 +0100

    Updates

---
+++ precache-manifest.3d139b12f22a4d91572d6bb15fb0c950.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc40211af9dd49072eb4fbff1aa7a04e",
+    "url": "/calc/index.html"
+  },
+  {
+    "revision": "77cd98c41dc163e27d0a",
+    "url": "/calc/static/css/main.80ab8425.chunk.css"
+  },
+  {
+    "revision": "fde96a308598f6d414c6",
+    "url": "/calc/static/js/2.08ff8035.chunk.js"
+  },
+  {
+    "revision": "77cd98c41dc163e27d0a",
+    "url": "/calc/static/js/main.3ae62060.chunk.js"
+  },
+  {
+    "revision": "76a64fcb6ff512077f0c",
+    "url": "/calc/static/js/runtime~main.dd744c6b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calc/static/media/logo.5d5d9eef.svg"
+  }
+]);
